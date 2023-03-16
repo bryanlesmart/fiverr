8a5881d39217893c4f47c03a3f26b1311b4d663a
venumaru <bryansilmaro03@gmail.com>
2023-03-16 18:08:24 +0800

    making a each gig

---
+++ src/routes/(gigs)/gig/[gigId]/+page.svelte
@@ -0,0 +1,570 @@
+<script lang="ts">
+	import { path } from '$lib/data';
+</script>
+
+<div class="gig">
+	<div class="container">
+		<div class="left">
+			<span class="breadcrumbs">Liver > Graphics & Design ></span>
+			<h1>I will create ai generated art for you</h1>
+			<div class="user">
+				<img
+					class="pp"
+					src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+					alt=""
+				/>
+				<span>Anna Bell</span>
+				<div class="stars">
+					<img src="{path}star.png" alt="" />
+					<img src="{path}star.png" alt="" />
+					<img src="{path}star.png" alt="" />
+					<img src="{path}star.png" alt="" />
+					<img src="{path}star.png" alt="" />
+					<span>5</span>
+				</div>
+			</div>
+			<div class="slider">
+				<img
+					src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+					alt=""
+				/>
+				<img
+					src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+					alt=""
+				/>
+				<img
+					src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
+					alt=""
+				/>
+			</div>
+			<h2>About This Gig</h2>
+			<p>
+				I use an AI program to create images based on text prompts. This means I can help you to
+				create a vision you have through a textual description of your scene without requiring any
+				reference images. Some things I've found it often excels at are: Character portraits (E.g. a
+				picture to go with your DnD character) Landscapes (E.g. wallpapers, illustrations to
+				compliment a story) Logos (E.g. E sports team, business, profile picture) You can be as
+				vague or as descriptive as you want. Being more vague will allow the AI to be more creative
+				which can sometimes result in some amazing images. You can also be incredibly precise if you
+				have a clear image of what you want in mind. All of the images I create are original and
+				will be found nowhere else. If you have any questions you're more than welcome to send me a
+				message.
+			</p>
+			<div class="seller">
+				<h2>About The Seller</h2>
+				<div class="user">
+					<img
+						src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+						alt=""
+					/>
+					<div class="info">
+						<span>Anna Bell</span>
+						<div class="stars">
+							<img src="{path}star.png" alt="" />
+							<img src="{path}star.png" alt="" />
+							<img src="{path}star.png" alt="" />
+							<img src="{path}star.png" alt="" />
+							<img src="{path}star.png" alt="" />
+							<span>5</span>
+						</div>
+						<button>Contact Me</button>
+					</div>
+				</div>
+				<div class="box">
+					<div class="items">
+						<div class="item">
+							<span class="title">From</span>
+							<span class="desc">USA</span>
+						</div>
+						<div class="item">
+							<span class="title">Member since</span>
+							<span class="desc">Aug 2022</span>
+						</div>
+						<div class="item">
+							<span class="title">Avg. response time</span>
+							<span class="desc">4 hours</span>
+						</div>
+						<div class="item">
+							<span class="title">Last delivery</span>
+							<span class="desc">1 day</span>
+						</div>
+						<div class="item">
+							<span class="title">Languages</span>
+							<span class="desc">English</span>
+						</div>
+					</div>
+					<hr />
+					<p>
+						My name is Anna, I enjoy creating AI generated art in my spare time. I have a lot of
+						experience using the AI program and that means I know what to prompt the AI with to get
+						a great and incredibly detailed result.
+					</p>
+				</div>
+			</div>
+			<div class="reviews">
+				<h2>Reviews</h2>
+				<div class="item">
+					<div class="user">
+						<img
+							class="pp"
+							src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
+							alt=""
+						/>
+						<div class="info">
+							<span>Garner David</span>
+							<div class="country">
+								<img
+									src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+									alt=""
+								/>
+								<span>United States</span>
+							</div>
+						</div>
+					</div>
+					<div class="stars">
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<span>5</span>
+					</div>
+					<p>
+						I just want to say that art_with_ai was the first, and after this, the only artist Ill
+						be using on Fiverr. Communication was amazing, each and every day he sent me images that
+						I was free to request changes to. They listened, understood, and delivered above and
+						beyond my expectations. I absolutely recommend this gig, and know already that Ill be
+						using it again very very soon
+					</p>
+					<div class="helpful">
+						<span>Helpful?</span>
+						<img src="{path}like.png" alt="" />
+						<span>Yes</span>
+						<img src="{path}dislike.png" alt="" />
+						<span>No</span>
+					</div>
+				</div>
+				<hr />
+				<div class="item">
+					<div class="user">
+						<img
+							class="pp"
+							src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
+							alt=""
+						/>
+						<div class="info">
+							<span>Sidney Owen</span>
+							<div class="country">
+								<img
+									src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
+									alt=""
+								/>
+								<span>Germany</span>
+							</div>
+						</div>
+					</div>
+					<div class="stars">
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<span>5</span>
+					</div>
+					<p>
+						The designer took my photo for my book cover to the next level! Professionalism and ease
+						of working with designer along with punctuality is above industry standards!! Whatever
+						your project is, you need this designer!
+					</p>
+					<div class="helpful">
+						<span>Helpful?</span>
+						<img src="{path}like.png" alt="" />
+						<span>Yes</span>
+						<img src="{path}dislike.png" alt="" />
+						<span>No</span>
+					</div>
+				</div>
+				<hr />
+				<div class="item">
+					<div class="user">
+						<img
+							class="pp"
+							src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
+							alt=""
+						/>
+						<div class="info">
+							<span>Lyle Giles </span>
+							<div class="country">
+								<img
+									src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+									alt=""
+								/>
+								<span>United States</span>
+							</div>
+						</div>
+					</div>
+					<div class="stars">
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<img src="{path}star.png" alt="" />
+						<span>5</span>
+					</div>
+					<p>
+						Amazing work! Communication was amazing, each and every day he sent me images that I was
+						free to request changes to. They listened, understood, and delivered above and beyond my
+						expectations. I absolutely recommend this gig, and know already that Ill be using it
+						again very very soon
+					</p>
+					<div class="helpful">
+						<span>Helpful?</span>
+						<img src="{path}like.png" alt="" />
+						<span>Yes</span>
+						<img src="{path}dislike.png" alt="" />
+						<span>No</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="right">
+			<div class="price">
+				<h3>1 AI generated image</h3>
+				<h2>$ 59.99</h2>
+			</div>
+			<p>
+				I will create a unique high quality AI generated image based on a description that you give
+				me
+			</p>
+			<div class="details">
+				<div class="item">
+					<img src="{path}clock.png" alt="" />
+					<span>2 Days Delivery</span>
+				</div>
+				<div class="item">
+					<img src="{path}recycle.png" alt="" />
+					<span>3 Revisions</span>
+				</div>
+			</div>
+			<div class="features">
+				<div class="item">
+					<img src="{path}greencheck.png" alt="" />
+					<span>Prompt writing</span>
+				</div>
+				<div class="item">
+					<img src="{path}greencheck.png" alt="" />
+					<span>Artwork delivery</span>
+				</div>
+				<div class="item">
+					<img src="{path}greencheck.png" alt="" />
+					<span>Images</span>
+				</div>
+				<div class="item">
+					<img src="{path}greencheck.png" alt="" />
+					<span>Additional design</span>
+				</div>
+			</div>
+			<a href="/pay" class="link btn">Continue</a>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.gig {
+		display: flex;
+		justify-content: center;
+
+		.container {
+			width: 1400px;
+			padding: 30px 0px;
+			display: flex;
+			gap: 50px;
+
+			.left {
+				flex: 2;
+				display: flex;
+				flex-direction: column;
+				gap: 20px;
+
+				.breadcrumbs {
+					font-weight: 300;
+					text-transform: uppercase;
+					font-size: 13px;
+					color: #555;
+				}
+
+				.user {
+					display: flex;
+					align-items: center;
+					gap: 10px;
+
+					.pp {
+						width: 32px;
+						height: 32px;
+						border-radius: 50%;
+						object-fit: cover;
+					}
+
+					span {
+						font-size: 14px;
+						font-weight: 500;
+					}
+
+					.stars {
+						display: flex;
+						align-items: center;
+						gap: 5px;
+
+						img {
+							height: 14px;
+							width: 14px;
+						}
+
+						span {
+							font-size: 14px;
+							font-weight: bold;
+							color: #ffc108;
+						}
+					}
+				}
+
+				.slider {
+					background-color: #f5f5f5;
+					img {
+						max-height: 500px;
+						object-fit: contain;
+					}
+				}
+
+				h2 {
+					font-weight: 400;
+				}
+
+				p {
+					font-weight: 300;
+					line-height: 25px;
+					color: #555;
+				}
+
+				.seller {
+					margin-top: 50px;
+					display: flex;
+					flex-direction: column;
+					gap: 20px;
+
+					.user {
+						display: flex;
+						align-items: center;
+						gap: 20px;
+
+						img {
+							width: 100px;
+							height: 100px;
+							border-radius: 50%;
+							object-fit: cover;
+						}
+
+						.info {
+							display: flex;
+							flex-direction: column;
+							gap: 10px;
+
+							.stars {
+								display: flex;
+								align-items: center;
+								gap: 5px;
+
+								img {
+									height: 14px;
+									width: 14px;
+								}
+
+								span {
+									font-size: 14px;
+									font-weight: bold;
+									color: #ffc108;
+								}
+							}
+
+							button {
+								background-color: white;
+								border-radius: 5px;
+								border: 1px solid gray;
+								padding: 10px;
+							}
+						}
+					}
+
+					.box {
+						border: 1px solid lightgray;
+						border-radius: 5px;
+						padding: 20px;
+						margin-top: 20px;
+
+						.items {
+							display: flex;
+							justify-content: space-between;
+							flex-wrap: wrap;
+
+							.item {
+								width: 300px;
+								display: flex;
+								flex-direction: column;
+								gap: 10px;
+								margin-bottom: 20px;
+
+								.title {
+									font-weight: 300;
+								}
+							}
+						}
+
+						hr {
+							height: 0;
+							border: 0.5px solid lightgray;
+							margin-bottom: 20px;
+						}
+					}
+				}
+
+				.reviews {
+					margin-top: 50px;
+					.item {
+						display: flex;
+						flex-direction: column;
+						gap: 20px;
+						margin: 20px 0px;
+						.user {
+							display: flex;
+							align-items: center;
+							.pp {
+								height: 50px;
+								width: 50px;
+								border-radius: 50%;
+							}
+
+							.country {
+								display: flex;
+								align-items: center;
+								gap: 10px;
+								color: gray;
+
+								img {
+									width: 20px;
+								}
+							}
+						}
+
+						.stars {
+							display: flex;
+							gap: 5px;
+
+							img {
+								height: 14px;
+								width: 14px;
+							}
+
+							span {
+								font-size: 14px;
+								font-weight: bold;
+								color: #ffc108;
+							}
+						}
+
+						.helpful {
+							display: flex;
+							align-items: center;
+							gap: 10px;
+
+							img {
+								width: 14px;
+							}
+						}
+					}
+
+					hr {
+						height: 0;
+						border: 0.5px solid lightgray;
+						margin: 50px 0px;
+					}
+				}
+			}
+
+			.right {
+				flex: 1;
+				border: 1px solid lightgray;
+				padding: 20px;
+				border-radius: 5px;
+				display: flex;
+				flex-direction: column;
+				gap: 20px;
+				height: max-content;
+				max-height: 500px;
+				position: sticky;
+				top: 150px;
+
+				.price {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+
+					h2 {
+						font-weight: 300;
+					}
+
+					h3 {
+						font-weight: 500;
+					}
+				}
+
+				p {
+					color: gray;
+					margin: 10px 0px;
+				}
+
+				.details {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					font-size: 14px;
+
+					.item {
+						display: flex;
+						align-items: center;
+						gap: 10px;
+
+						img {
+							width: 20px;
+						}
+					}
+				}
+
+				.features {
+					.item {
+						display: flex;
+						align-items: center;
+						gap: 10px;
+						font-weight: 300;
+						color: gray;
+						margin-bottom: 5px;
+
+						img {
+							width: 14px;
+						}
+					}
+				}
+
+				.btn {
+					background-color: #1dbf73;
+					padding: 10px;
+					color: white;
+					font-weight: 500;
+					border: none;
+					font-size: 18px;
+					cursor: pointer;
+					text-align: center;
+				}
+			}
+		}
+	}
+</style>
